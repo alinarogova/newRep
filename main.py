95de0bd1f5d97c875a5ad07b0005ccc4e1063622
Alina <alinarogova1991@gmail.com>
2024-10-07 19:30:29 +0300

    Create function load_tasks

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-def 
+FILENAME = 'tasks.txt'
+def load_tasks(FILENAME):
+    tasks = []
+    with open(FILENAME, 'r', encoding="utf-8") as fl:
+        for line in fl.readlines():
+            tasks.append(line.split())
+
+def save_tasks(tasks):
+    with open(FILENAME, 'w', encoding="utf-8"):
+        pass
+
+def add_tasks(tasks, task):
